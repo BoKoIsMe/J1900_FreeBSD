59a44545d3cc7a2a3a26e8ec441dc1c7a336caab
BoKoIsMe <typebackup@hotmail.com>
2019-01-06 11:08:30 +0800

    修正了一些BUG,但是还有是有BUG syscall.write 使用第二次就无输出 原因不明

--- asm/CPUID/hello32/hello32.asm
+++ asm/CPUID/hello32/hello32.asm
@@ -13,37 +13,29 @@ CPUID_STATUS_FAILURE	EQU 1
 
 section .data
 ;数据段
+	ttyname db "./1.txt",0
+	tty_fd	dq 0
 	msg		db "0x00000000",10,0		;字串
 	hbyte0	equ $ - msg
+	msg1	db "Start run CPUID & rdrand",10,0	;字串2
+	hbyte1	equ $ - msg1
 
 section .text
 ;代码段
 global _start
-
-kernel:
-	int 80h
-	ret
-
-hex2ascii:
-	push dword ecx
-	push dword edx
-	and dl,0fh
-	add dl,30h
-	cmp dl,'9'
-	jbe .next1
-	add dl,7
-.next1:
-	mov al,dl
-	pop dword edx
-	pop dword ecx
-	ret
-
 _start:
+	push hbyte1
+	push msg1
+	push STDOUT
+	mov eax,AUE_WRITE
+	call kernel
+
 	mov eax,1
 	cpuid
 	bt ecx,30;位测试失败则CPU不支持真随机数
 	mov ebx,CPUID_STATUS_FAILURE;检测失败退出代码
 	jnc .exit
+
 ;如果无随机数可用,rdrand执行后返回置标志位 CF = 0
 ;根据Intel文档建议 测试10次以上
 	mov ecx,11
@@ -54,15 +46,20 @@ _start:
 	jnc .loop1
 
 ; HEX2ASCII
-	mov edi,msg+9
-	mov ecx,8
-	mov edx,eax
-.loop2:
-	call hex2ascii
-	mov byte [edi],al
-	shr edx,4
-	dec edi
-	loop .loop2
+	;mov rdi,msg+9
+	;mov ecx,8
+;.loop2:
+	;mov edx,eax
+	;and edx,0FH
+	;mov r8d,7
+	;xor r9d,r9d;清零
+	;cmp dl,9
+	;cmova r9,r8
+	;add edx,r9d
+	;add [rdi],dl
+	;shr eax,4
+	;dec edi
+	;loop .loop2
 
 	push hbyte0
 	push msg
@@ -75,4 +72,8 @@ _start:
 	mov eax,AUE_EXIT
 	push ebx
 	call kernel
+
+kernel:
+	int 80h
+	ret
 
